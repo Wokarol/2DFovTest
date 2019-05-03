de6e3017d997d153458535717109fa480212d5e6
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2019-05-03 15:57:58 +0200

    Added Terminal and started debuging statics

--- StealthThiefGame/Assets/Game/Scripts/Runtime/ObservationSystem/ObstacleManager.cs
+++ StealthThiefGame/Assets/Game/Scripts/Runtime/ObservationSystem/ObstacleManager.cs
@@ -3,19 +3,39 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
 
 public static class ObstacleManager
 {
-    static List<Vector2> corners = new List<Vector2>();
-    public static IReadOnlyList<Vector2> Corners => corners;
+    static long lastSession = 0;
 
+    static List<Vector2> corners = new List<Vector2>();
+    public static IReadOnlyList<Vector2> Corners {
+        get {
+            if ((Application.isPlaying && lastSession != AnalyticsSessionInfo.sessionId) || true) {
+                lastSession = AnalyticsSessionInfo.sessionId;
+                ForceObstacleRefresh();
+            }
+            return corners;
+        }
+    }
+
+
+
+    //[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
     public static void ForceObstacleRefresh() {
+        Debug.Log($"Refreshed obstacles for session {lastSession} and scene {SceneManager.GetActiveScene().buildIndex}");
+
         var colliders = UnityEngine.Object.FindObjectsOfType<Collider2D>().Where(c => c.gameObject.isStatic).ToArray();
         corners.Clear();
 
         foreach (var c in colliders) {
             corners.AddRange(GetCorners(c));
         }
+
+        Debug.Log(colliders.Length);
+        Debug.Log(UnityEngine.Object.FindObjectsOfType<Collider2D>().Length);
     }
 
     private static Vector2[] GetCorners(Collider2D c) {
